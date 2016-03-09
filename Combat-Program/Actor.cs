837a2281d15d33c5cc1b29d1af6a42254cd4efe3
Austin Morrell <austinlm19@gmail.com>
2016-03-08 18:06:32 -0600

    First commit

---
+++ Combat-Program/Actor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Combat_Program
+{
+    class Actor : ICanTakeActions
+    {
+        protected float m_HP;
+        protected float m_Attack;
+        protected float m_Defence;
+        protected string m_Status;
+    }
+}
